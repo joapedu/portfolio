e99a9ca85abb0b83225836d483f849007bb0721f
joapedu <joaoeduardobraga2@gmail.com>
2024-06-03 16:48:16 -0300

    :sparkles: feat: final utils to ts archives

---
+++ src/utils/contentful.ts
@@ -0,0 +1,93 @@
+import { cache } from 'react';
+
+import { Asset, createClient } from 'contentful';
+
+import { ContentSkeleton, ProjectSkeleton, ProjectsSkeleton } from '@/types';
+
+import { getBase64 } from './getBase64';
+
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID!,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+  environment: process.env.CONTENTFUL_ENVIROMENT || 'master'
+});
+
+export const getAllProjects = cache(async () => {
+  try {
+    const { items } = await client.getEntries<ProjectsSkeleton>({
+      content_type: 'projects',
+      order: ['fields.index'],
+      select: ['fields.name', 'fields.slug', 'fields.tags', 'fields.image']
+    });
+
+    const projects = await Promise.all(
+      items.map(async ({ fields }) => {
+        const image = fields?.image as Asset<undefined, string>;
+        const imgSrc = image ? `https:${image?.fields?.file?.url}` : '';
+        const imgBase64 = await getBase64(imgSrc);
+
+        return {
+          name: fields.name,
+          slug: fields.slug,
+          tags: fields.tags,
+          image: {
+            src: imgSrc,
+            alt: image ? image.fields?.description : fields.name,
+            base64: imgBase64
+          }
+        };
+      })
+    );
+
+    return projects;
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+export const getSingleProject = cache(async (slug_: string) => {
+  try {
+    const { items } = await client.getEntries<ProjectSkeleton>({
+      content_type: 'projects',
+      'fields.slug': slug_
+    });
+
+    const { image, ...rest } = items[0].fields;
+    const img = image as Asset<undefined, string>;
+    const imgSrc = img ? `https:${img.fields?.file?.url}` : '';
+    const imgBase64 = await getBase64(imgSrc);
+
+    return {
+      ...rest,
+      image: {
+        src: img ? `https:${img.fields?.file?.url}` : '',
+        alt: img ? img.fields.description : rest.name,
+        base64: imgBase64
+      }
+    };
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+export const getContent = cache(async () => {
+  try {
+    const { items: content } = await client.getEntries<ContentSkeleton>({
+      content_type: 'content'
+    });
+
+    return {
+      about: content[0].fields.about?.split(/\r?\n/).filter((line) => line.trim() !== '') || [],
+      skills: content[0].fields.skills || [],
+      navLinks: content[0].fields.navLinks?.data || {},
+      contactLinks: content[0].fields.contacts || {},
+      otherLinks: content[0].fields.otherLinks || {}
+    };
+  } catch (error) {
+    console.error(error);
+  }
+});
